481010b70cf390d85507f81dae1f729de8baf9ce
ad0v0 <olha.vadiasova@gmail.com>
2023-02-16 20:22:34 +0200

    Progress update

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,1 +1,11 @@
-console.log('Your code goes here!');
+function add(num1: number, num2: number) {
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    throw new Error('incorrect input!')
+  }
+  return num1 + num2
+}
+
+const num1 = '2'
+const num2 = 2.5
+const result = add(num1, num2)
+console.log(result)
